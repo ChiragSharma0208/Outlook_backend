bc1c350e6186c064d3992bd41de46dae36eaea0a
Chirag Sharma <85753492+ChiragSharma0208@users.noreply.github.com>
2025-02-16 13:22:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from datetime import datetime
 from dotenv import load_dotenv
 import requests
 from flask_cors import CORS 
+from flask_session import Session
+import redis
 from db_operations import save_user_info, save_user_emails, get_emails
 from auth.oauth2 import get_access_token, get_user_info, get_user_emails  
 
@@ -12,12 +14,23 @@ load_dotenv()
 app = Flask(__name__)
 app.secret_key = os.getenv('FLASK_SECRET_KEY')
 
-
 CLIENT_ID = os.getenv('CLIENT_ID')
 CLIENT_SECRET = os.getenv('CLIENT_SECRET')
 REDIRECT_URI = os.getenv('REDIRECT_URI')
 FRONTEND = os.getenv('FRONTEND')
 CORS(app, origins=[f"{FRONTEND}"], supports_credentials=True)
+
+# Configure Flask-Session with Redis
+app.config["SESSION_TYPE"] = "redis"
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_USE_SIGNER"] = True
+app.config["SESSION_KEY_PREFIX"] = "flask-session:"
+app.config["SESSION_REDIS"] = redis.StrictRedis(host='your-redis-host', port=6379, db=0, decode_responses=True)
+app.config["SESSION_COOKIE_SECURE"] = True  # Ensures cookies are only sent over HTTPS
+app.config["SESSION_COOKIE_HTTPONLY"] = True  # Prevents JavaScript from accessing session cookies
+app.config["SESSION_COOKIE_SAMESITE"] = "Lax"
+
+Session(app)
 
 @app.route("/")
 def home():
@@ -42,10 +55,9 @@ def callback():
 
         session["access_token"] = access_token
         
-
         user_info = get_user_info(access_token)
         user_id = user_info['id']
-        session["user_id"]=user_id
+        session["user_id"] = user_id
         save_user_info(user_info)
 
         user_emails = get_user_emails(access_token)
@@ -58,12 +70,13 @@ def callback():
     
 @app.route('/update')
 def update():
+    print(session)  # Debugging statement
     access_token = session.get("access_token")
     if not access_token:
         return "Error: Access token not found.", 500
     user_id = session.get("user_id")
     if not user_id:
-        return "Error: Access token not found.", 500
+        return "Error: User ID not found.", 500
     user_emails = get_user_emails(access_token)
     save_user_emails(user_emails, user_id)
     try:
@@ -72,7 +85,6 @@ def update():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-    
 @app.route('/logout', methods=['POST'])
 def logout():
     session.clear()
@@ -92,12 +104,3 @@ def emails():
     
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
-
-
-
-
-
-
-
-
-
